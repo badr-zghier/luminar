b64032903c178d74462028430c8abf8fcc4f0128
BADR <badralzghier@gmail.com>
2024-05-27 19:34:38 +0300

    modify RegisteredUsersControllers

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified', 'role:teacher'])->name('dashboard');
 
+
+
+
 Route::middleware(['auth', 'role:teacher'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
